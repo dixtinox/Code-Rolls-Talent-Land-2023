fe6b523ec372dfaf78b79e3eaca4c96cbeef0059
Derek up <up200424@alumnos.upa.edu.mx>
2023-04-10 14:19:56 -0600

    feat: mongodb connection

--- BackEND/server.js
+++ BackEND/server.js
@@ -1,14 +1,19 @@
 //Server
+require('dotenv').config();
 const app = require('express')();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
+const mongoose = require('mongoose');
+const connectDB = require('./src/config/dbConn');
 
-app.listen(
-    port,
-    () => console.log(`Server is listening on http://localhost:${port}`)
-)
+connectDB();
 
 app.get('/', (req, res) => {
 
     res.send('Hello World');
 
+});
+
+mongoose.connection.once('open', () => {
+    console.log('Connected to MongoDB');
+    app.listen(port, () => console.log(`Server running on port ${port}`));
 });
