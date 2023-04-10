11ca5b83154303aa19f0f727634abded9099a46d
TunasFC <ivanibarra742@gmail.com>
2023-04-10 13:29:16 -0600

    update server.js
    se creo una coneccion con el server

--- BackEND/server.js
+++ BackEND/server.js
@@ -1,1 +1,14 @@
 //Server
+const app = require('express')();
+const port = process.env.PORT || 3000;
+
+app.listen(
+    port,
+    () => console.log(`Server is listening on http://localhost:${port}`)
+)
+
+app.get('/', (req, res) => {
+
+    res.send('Hello World');
+
+});
