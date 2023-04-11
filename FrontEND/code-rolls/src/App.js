15456c188f408acab1069e4d011b369f3b4e9d96
LuisPerezLec <luigiperez2002@gmail.com>
2023-04-11 10:28:17 -0600

    Login semifuncional

--- FrontEND/code-rolls/src/App.js
+++ FrontEND/code-rolls/src/App.js
@@ -4,10 +4,19 @@ import { Space } from "antd";
 import AppFooter from './components/Footer'
 import AppHeader from './components/Header'
 import Content from './components/Content'
+import LoginPage from './components/Login'
+
 
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 function App() {
+
+  const user = localStorage.getItem('user');
+
+  if (!user) {
+    console.log('Existe usuario')
+    return <LoginPage />;
+  }
   return (
     <div className='App'>
       <AppHeader />
