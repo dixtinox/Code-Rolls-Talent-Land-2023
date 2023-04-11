1823ad89705a413d7fd0e691b0ec414f078ae173
Derek up <up200424@alumnos.upa.edu.mx>
2023-04-10 18:23:22 -0600

    singing

---
+++ BackEND/src/routes/auth_router.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+const controller = require('../controllers/auth_controller.js');
+
+router.route('/singin').post(controller.singin);
+// router.route('/login').get(controller.test);
+
+
+module.exports = router;
