8a1494854c4d9269fbdb34bf6d9eb7b4b792b558
LuisPerezLec <luigiperez2002@gmail.com>
2023-04-12 12:27:14 -0600

    Dashboar mid-way

--- FrontEND/code-rolls/src/components/DashboardComponents/DayAchivement.js
+++ FrontEND/code-rolls/src/components/DashboardComponents/DayAchivement.js
@@ -1,21 +1,19 @@
 import React from 'react'
 
-const DayAchivement = ({ colorA, colorB, colorC }) => {
+const DayAchivement = ({ arregloColor }) => {
   return (
     <div style={{ 
         width: "50px", 
         height: "50px", 
         borderRadius: "50%",
-        background: `conic-gradient(${colorA} 0, ${colorA} 120deg, ${colorB} 120deg, ${colorB} 240deg, ${colorC} 240deg)` 
+        background: `conic-gradient(${arregloColor[0]} 0, ${arregloColor[0]} 120deg, ${arregloColor[1]} 120deg, ${arregloColor[1]} 240deg, ${arregloColor[2]} 240deg)` 
       }}>
       </div>
   )
 }
 
 DayAchivement.defaultProps = {
-    colorA:"#808080",
-    colorB:"#808080",
-    colorC:"#808080", 
+  arregloColor: ["#808080","#808080","#808080"]
 }
 
 export default DayAchivement
--- FrontEND/code-rolls/src/components/Pages/Dashboard.js
+++ FrontEND/code-rolls/src/components/Pages/Dashboard.js
@@ -1,11 +1,43 @@
 import Streak from '../DashboardComponents/Streak'
+import PerfectDays from '../DashboardComponents/PerfectDays'
+import { useEffect, useState } from 'react'
 import 'bootstrap/dist/css/bootstrap.css';
+import axios from 'axios';
 
 function Dashboard(){
+    const [iniDate, setIniDate] = useState('');
+    const [obj, setObj] = useState([]);
+    const [perf, setPerf] = useState();
+    const [points, setPoints] = useState();
+    const [current, setCurrent] = useState();
+    const [receivedData, setReceivedData] = useState(false);
+
+    async function getDashboardData() {
+        const response = await axios.post('http://localhost:3000/objectives/checkstreak', {
+            user_id: '6436434ec1c5ad4d6023a8c0'
+        })
+        const { user: {initialStreakDay, perfectDays, nutriPoints, objectives}, currentDay} = response.data;
+        setIniDate(initialStreakDay);
+        setObj(objectives);
+        setPerf(perfectDays);
+        setPoints(nutriPoints);
+        setCurrent(currentDay);
+        setReceivedData(true);
+    }
+
+    useEffect(() => {
+        getDashboardData();
+    }, [])
+
+    if (!receivedData) {
+        return <div>Loading...</div>;
+    }
+
     return <div>Dashboard
-        <Streak title="Toma al menos 2L de agua" buttonText="Listo!" containerColor="green"/>
-        <Streak title="Come al menos 500 calorias" buttonText="Listo!" containerColor="blue"/>
-        <Streak title="Haz al menos 20min de actividad física" buttonText="Listo!" containerColor="orange"/>
+        <PerfectDays title="Días de racha" containerColor="black" valores={obj} streak={0} reload={getDashboardData}/>
+        <Streak title="Toma al menos 2L de agua" buttonText="Listo!" containerColor="green" valores={obj} streak={0} reload={getDashboardData}/>
+        <Streak title="Come al menos 500 calorias" buttonText="Listo!" containerColor="blue" valores={obj} streak={1} reload={getDashboardData}/>
+        <Streak title="Haz al menos 20min de actividad física" buttonText="Listo!" containerColor="orange" valores={obj} streak={2} reload={getDashboardData}/>
     </div>
 }
 
