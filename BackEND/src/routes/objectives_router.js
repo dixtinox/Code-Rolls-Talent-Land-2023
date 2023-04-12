0daae1cf347f7af14811effa5778c81dcd9653e7
Derek up <up200424@alumnos.upa.edu.mx>
2023-04-11 19:23:15 -0600

    objectives mark upgrade

--- BackEND/src/routes/objectives_router.js
+++ BackEND/src/routes/objectives_router.js
@@ -5,7 +5,7 @@ const controller = require('../controllers/objectives_controller.js');
 router.route('/checkstreak').get(controller.CheckDailyStreak);
 router.route('/followDiet').put(controller.CompleteDailyStreakFollowDiet);
 router.route('/drinkWater').put(controller.CompleteDailyStreakDrink);
-router.route('/exetcise').put(controller.CompleteDailyStreakExcercise);
+router.route('/exercise').put(controller.CompleteDailyStreakExcercise);
 
 
 module.exports = router;
