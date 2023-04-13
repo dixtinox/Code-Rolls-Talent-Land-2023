7e71bbe565f1032045cc2c82436eca71cfd55020
LuisPerezLec <luigiperez2002@gmail.com>
2023-04-12 22:40:46 -0600

    Alerta por racha

--- FrontEND/code-rolls/src/components/Pages/Dashboard.js
+++ FrontEND/code-rolls/src/components/Pages/Dashboard.js
@@ -2,13 +2,25 @@ import Streak from '../DashboardComponents/Streak'
 import PerfectDays from '../DashboardComponents/PerfectDays'
 import DonutChart from '../DashboardComponents/DonutChart'
 import NutriPoints from '../DashboardComponents/NutriPoints'
+import Nutri from '../components-Nutri/Nutri'
 import { useEffect, useState } from 'react'
-import { Card, Space } from 'antd';
+import { Card, Space, Row, Col } from 'antd';
 import 'animate.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import axios from 'axios';
 
 function Dashboard(){
+    const [activePerfectWeek, setActivePerfectWeek] = useState(false);
+    const [activePerfectDay, setActivePerfectDay] = useState(false);
+
+  const toggle = () => {
+    setActivePerfectDay(!activePerfectDay)
+  }
+
+  const toggle1 = () => {
+    setActivePerfectWeek(!activePerfectWeek)
+  }
+
     const [iniDate, setIniDate] = useState('');
     const [obj, setObj] = useState([]);
     const [perf, setPerf] = useState();
@@ -58,16 +70,12 @@ function Dashboard(){
     }, [points])
 
     useEffect(()=> {
-        setAnimando2(true);
-        
-        setTimeout(() => {
-          setAnimando2(false);
-        }, 1000);
+        toggle();
     }, [perf])
 
     useEffect(() => {
         if (streakArray[0] && streakArray[1] && streakArray[2] && streakArray[3] && streakArray[4] && streakArray[5] && streakArray[6]){
-            console.log("Felicidades, semana perfecta")
+            toggle1();
         }
     }, [streakArray])
 
@@ -76,6 +84,20 @@ function Dashboard(){
     }
 
     return <div className='d-flex p-5'>
+        <Nutri
+            active={activePerfectDay}
+            toggle={toggle}
+            imagen={0}
+            class="nutri--containerCircular"
+            descripcion={`Felicidades! Cumpliste con todos tus objetivos diarios!`}
+        ></Nutri>
+        <Nutri
+            active={activePerfectWeek}
+            toggle={toggle1}
+            imagen={0}
+            class="nutri--containerCircular"
+            descripcion={`Felicidades! Cumpliste con tus objetivos durante toda una semana`}
+        ></Nutri>
         <Card>
             <PerfectDays title="Días de racha" containerColor="darkslateblue" valores={obj} streak={0} reload={getDashboardData}/>
             <Streak title="Toma al menos 2L de agua" buttonText="Listo!" containerColor="silver" valores={obj} streak={0} reload={getDashboardData}/>
@@ -83,7 +105,9 @@ function Dashboard(){
             <Streak title="Haz al menos 20min de actividad física" buttonText="Listo!" containerColor="silver" valores={obj} streak={2} reload={getDashboardData} />
         </Card>
         <Card>
-            <NutriPoints title={["NutriPoints", "Días de racha"]} containerColor="silver" points={points} perfectDays={perf} animar={[animando1, animando2]}/>
+            <div className='d-flex justify-content-center'>
+                <NutriPoints title={["NutriPoints", "Días perfectos"]} containerColor="silver" points={points} perfectDays={perf} animar={[animando1, animando2]}/>
+            </div>
             <DonutChart streakArray={streakArray}/>
         </Card>
     </div>
